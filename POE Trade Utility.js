10982ef0422397841b0fe59f9aee9acbe77e0553
Leo Moreno <merlin8213@gmail.com>
2025-02-19 11:40:30 -0700

    Bump version to 0.0.2 in POE Trade Utility.js

--- POE Trade Utility.js
+++ POE Trade Utility.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         POE2 Trade Utility (Dev)
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
+// @version      0.0.2
 // @description  A utility to help with trading in Path of Exile. Currently helps calculate the weighted sum of a item.
 // @author       EphemeralDust
 // @match        https://www.pathofexile.com/trade2/*
